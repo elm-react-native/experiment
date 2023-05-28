7fa532a953b70c90d3ecbec785747a658ebb5879
chendesheng <ichendesheng@gmail.com>
2023-05-28 18:00:27 +0800

    packages: add menuAttributes property to ios context menu

--- packages/react-native-ios-context-menu/ReactNative/ContextMenuIOS.elm
+++ packages/react-native-ios-context-menu/ReactNative/ContextMenuIOS.elm
@@ -1,4 +1,4 @@
-module ReactNative.ContextMenuIOS exposing (MenuItem, contextMenuButton, enableContextMenu, isMenuPrimaryAction, menuConfig, menuItemDecoder, onPressMenuItem, pressEventMenuItemDecoder, useActionSheetFallback)
+module ReactNative.ContextMenuIOS exposing (MenuItem, MenuItemAttribute(..), contextMenuButton, enableContextMenu, isMenuPrimaryAction, menuConfig, menuItemDecoder, onPressMenuItem, pressEventMenuItemDecoder, useActionSheetFallback)
 
 import Browser
 import Html exposing (Attribute, Html)
@@ -17,15 +17,67 @@ onPressMenuItem =
     on "pressMenuItem"
 
 
+type MenuItemAttribute
+    = Disabled
+    | Hidden
+    | Destructive
+    | KeepsMenuPresented
+
+
 type alias MenuItem =
-    { actionKey : String, actionTitle : String }
+    { actionKey : String, actionTitle : String, attributes : Maybe (List MenuItemAttribute) }
+
+
+menuItemAttributeEncoder : MenuItemAttribute -> Encode.Value
+menuItemAttributeEncoder attr =
+    case attr of
+        Disabled ->
+            Encode.string "disabled"
+
+        Hidden ->
+            Encode.string "hidden"
+
+        Destructive ->
+            Encode.string "destructive"
+
+        KeepsMenuPresented ->
+            Encode.string "keepsMenuPresented"
+
+
+menuItemAttributeDecoder : Decoder (Maybe MenuItemAttribute)
+menuItemAttributeDecoder =
+    let
+        fromStr attr =
+            case attr of
+                "disabled" ->
+                    Just Disabled
+
+                "hidden" ->
+                    Just Hidden
+
+                "destructive" ->
+                    Just Destructive
+
+                "keepsMenuPresented" ->
+                    Just KeepsMenuPresented
+
+                _ ->
+                    Nothing
+    in
+    Decode.map fromStr Decode.string
+
+
+listOfMaybe : Decoder (Maybe a) -> Decoder (List a)
+listOfMaybe =
+    Decode.map (List.filterMap identity) << Decode.list
 
 
 menuItemDecoder : Decoder MenuItem
 menuItemDecoder =
-    Decode.map2 MenuItem
+    Decode.map3 MenuItem
         (Decode.field "actionKey" Decode.string)
         (Decode.field "actionTitle" Decode.string)
+        (Decode.maybe <| Decode.field "menuAttributes" (listOfMaybe menuItemAttributeDecoder))
 
 
 pressEventMenuItemDecoder : Decoder MenuItem
@@ -38,7 +90,24 @@ menuConfig cfg =
     property "menuConfig" <|
         Encode.object
             [ ( "menuTitle", Encode.string cfg.menuTitle )
-            , ( "menuItems", Encode.list encode cfg.menuItems )
+            , ( "menuItems"
+              , Encode.list
+                    (\item ->
+                        Encode.object
+                            [ ( "actionKey", Encode.string item.actionKey )
+                            , ( "actionTitle", Encode.string item.actionTitle )
+                            , ( "menuAttributes"
+                              , case item.attributes of
+                                    Just attrs ->
+                                        Encode.list menuItemAttributeEncoder attrs
+
+                                    _ ->
+                                        Encode.null
+                              )
+                            ]
+                    )
+                    cfg.menuItems
+              )
             ]
 
 
